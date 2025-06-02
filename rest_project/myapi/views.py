d14cc173dc25d15f1dabdbd6be2faeca245dcb0c
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-06-02 21:31:38 +0530

    This is new commit with post

--- rest_project/myapi/views.py
+++ rest_project/myapi/views.py
@@ -3,9 +3,17 @@ from .models import Item
 from .serializers import ItemSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def get_items(request):
-    items = Item.objects.all()
-    serializers = ItemSerializer(items, many=True)
-    return Response(serializers.data)
+    if request.method == 'GET':
+        items = Item.objects.all()
+        serializers = ItemSerializer(items, many=True)
+        return Response(serializers.data)
+    elif request.method == 'POST':
+        serializers = ItemSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_200_OK)
+        return Response(serializers.data, status=status.HTTP_400_BAD_REQUEST)
