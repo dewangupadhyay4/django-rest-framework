1f91d0fc8bde6d95dea1a78201f1dd6a78e89609
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-06-03 07:17:14 +0530

    this is day 40

--- rest_project/myapi/urls.py
+++ rest_project/myapi/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 from .views import get_items
 
 urlpatterns = [
     path('items/',get_items,name="get_items"),
+    path('items/<int:pk>/',views.item_detail,name="item_details"),
 ]
