b64c588a1c10da9054a1e961d442d00953409811
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-06-08 17:05:48 +0530

    class based views

--- rest_project/myapi/views.py
+++ rest_project/myapi/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
@@ -65,3 +66,36 @@ class CustomAuthToken(ObtainAuthToken):
 @permission_classes([IsAuthenticated])
 def protected_view(request):
     return Response({'message':' Hello you are authenticated'})
+
+class ItemListView(APIView):
+    def get(self, request):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ItemSerializer(dadta=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class ItemDetailsView(APIView):
+    def get_object(self, pk):
+        item = set.get_object(pk)
+        if not item:
+            return Response({'error':'Item not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ItemSerializer(item)
+        return Response(serializer.data)
+    
+    def put(self,request, pk):
+        item = self.get_object(pk)
+        if not item:
+            return Response({'error':'Not found'},status=status.HTTP_404_NOT_FOUND)
+        serializer = ItemSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete
