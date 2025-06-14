dcd12f422046439f0ebac323709987f42881e3c8
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-06-14 21:40:10 +0530

    this is implementation of throttling

--- rest_project/rest_project/settings.py
+++ rest_project/rest_project/settings.py
@@ -128,7 +128,16 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    'DEFAULT_THROTTLE_CLASSES':[
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
     ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ],
+    'DEFAULT_THROTTLE_RATES': [
+        'anon':'5/minutes',
+        'user':'10/minutes'
+    ]
+
 }
