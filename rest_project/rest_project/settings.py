91fdb3f88fc6d2781610c4588a9346f11c15ee42
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-06-18 07:08:06 +0530

    this is pagination session

--- rest_project/rest_project/settings.py
+++ rest_project/rest_project/settings.py
@@ -148,5 +148,9 @@ REST_FRAMEWORK = {
         'user':'10/minute'
     },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5
+
 }
